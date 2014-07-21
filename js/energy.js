1bb91dbcf4ec0c80c257e53e40ba7aaeff66b1e4
DrMothra <tony.glover@nottingham.ac.uk>
2014-07-21 16:09:37 +0100

    Initial commit
    Construct framework and grid, etc.

---
+++ js/energy.js
@@ -0,0 +1,240 @@
+/**
+ * Created by atg on 21/07/2014.
+ */
+
+//Globals
+var GROUND_DEPTH = 240;
+var GROUND_WIDTH = 180;
+
+//Init this app from base
+function EnergyApp() {
+    BaseApp.call(this);
+}
+
+EnergyApp.prototype = new BaseApp();
+
+EnergyApp.prototype.init = function(container) {
+    BaseApp.prototype.init.call(this, container);
+    this.data = null;
+    this.updateRequired = false;
+    this.guiControls = null;
+    this.dataFile = null;
+    this.filename = '';
+    this.objectsRendered = 0;
+};
+
+EnergyApp.prototype.update = function() {
+    //Perform any updates
+    var clicked = this.mouse.clicked;
+
+    //Perform mouse hover
+    var vector = new THREE.Vector3(( this.mouse.x / window.innerWidth ) * 2 - 1, -( this.mouse.y / window.innerHeight ) * 2 + 1, 0.5);
+    this.projector.unprojectVector(vector, this.camera);
+
+    var raycaster = new THREE.Raycaster(this.camera.position, vector.sub(this.camera.position).normalize());
+
+    this.hoverObjects.length = 0;
+    this.hoverObjects = raycaster.intersectObjects(this.scene.children, true);
+
+    //Check hover actions
+    if(this.hoverObjects.length != 0) {
+        for(var i=0; i<this.hoverObjects.length; ++i) {
+
+        }
+    }
+    BaseApp.prototype.update.call(this);
+};
+
+EnergyApp.prototype.createScene = function() {
+    //Init base createsScene
+    BaseApp.prototype.createScene.call(this);
+
+    //Create ground
+    addGroundPlane(this.scene, GROUND_WIDTH, GROUND_DEPTH);
+};
+
+EnergyApp.prototype.createGUI = function() {
+    //Create GUI - use dat.GUI for now
+    this.guiControls = new function() {
+        this.filename = '';
+
+        //Colours
+        this.Ground = '#16283c';
+        this.Background = '#5c5f64';
+    };
+
+    //Create GUI
+    var gui = new dat.GUI();
+    var _this = this;
+    //Create two folders - Appearance and Data
+    gui.add(this.guiControls, 'filename', this.filename).listen();
+    this.guiAppear = gui.addFolder("Appearance");
+
+    this.guiAppear.addColor(this.guiControls, 'Ground').onChange(function(value) {
+        _this.groundColourChanged(value);
+    });
+    this.guiAppear.addColor(this.guiControls, 'Background').onChange(function(value) {
+        _this.renderer.setClearColor(value, 1.0);
+    });
+    this.guiData = gui.addFolder("Data");
+};
+
+EnergyApp.prototype.groundColourChanged = function(value) {
+    var ground = this.scene.getObjectByName('ground');
+    if(ground) {
+        ground.material.color.setStyle(value);
+    }
+};
+
+EnergyApp.prototype.parseFile = function() {
+    //Attempt to load and parse given json file
+    if(!this.filename) return;
+
+    console.log("Reading file...");
+
+    var reader = new FileReader();
+    var _this = this;
+    reader.onload = function(evt) {
+        //File loaded - parse it
+        console.log('file read: '+evt.target.result);
+        try {
+            _this.data = JSON.parse(evt.target.result);
+        }
+        catch (err) {
+            console.log('error parsing JSON file', err);
+            alert('Sorry, there was a problem reading that file');
+            return;
+        }
+        //File parsed OK - generate GUI controls and data
+        _this.generateGUIControls();
+        _this.generateData();
+        _this.updateRequired = true;
+    };
+
+    // Read in the file
+    reader.readAsText(this.dataFile, 'ISO-8859-1');
+};
+
+EnergyApp.prototype.onSelectFile = function(evt) {
+    //User selected file
+    //See if we support filereader API's
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        //File APIs are supported.
+        var files = evt.target.files; // FileList object
+        if (files.length==0) {
+            console.log('no file specified');
+            this.filename = "";
+            return;
+        }
+        //Clear old data first
+        if(this.dataFile) {
+            this.reset();
+        }
+        this.dataFile = files[0];
+        this.filename = this.dataFile.name;
+        console.log("File chosen", this.filename);
+
+        //Try and read this file
+        this.parseFile();
+    }
+    else
+        alert('sorry, file apis not supported');
+};
+
+function addGroundPlane(scene, width, height) {
+    // create the ground plane
+    var planeGeometry = new THREE.PlaneGeometry(width,height,1,1);
+    var texture = THREE.ImageUtils.loadTexture("images/grid.png");
+    var planeMaterial = new THREE.MeshLambertMaterial({map: texture, transparent: true, opacity: 0.5});
+    var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+
+    //plane.receiveShadow  = true;
+
+    // rotate and position the plane
+    plane.rotation.x=-0.5*Math.PI;
+    plane.position.x=0;
+    plane.position.y=-59.9;
+    plane.position.z=0;
+
+    scene.add(plane);
+
+    //Second plane
+    planeGeometry = new THREE.PlaneGeometry(width, height, 1, 1);
+    planeMaterial = new THREE.MeshLambertMaterial({color: 0x16283c});
+    plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x=-0.5*Math.PI;
+    plane.position.x=0;
+    plane.position.y=-60;
+    plane.position.z=0;
+    //Give it a name
+    plane.name = 'ground';
+
+    // add the plane to the scene
+    scene.add(plane);
+}
+
+function createLabel(name, position, scale, colour, fontSize, opacity) {
+
+    var fontface = "Arial";
+    var spacing = 10;
+
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
+    var metrics = context.measureText( name );
+    var textWidth = metrics.width;
+
+    canvas.width = textWidth + (spacing * 2);
+    canvas.width *= 2;
+    canvas.height = fontSize;
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+
+    context.fillStyle = "rgba(255, 255, 255, 0.0)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var red = Math.round(colour[0]);
+    var green = Math.round(colour[1]);
+    var blue = Math.round(colour[2]);
+
+    context.fillStyle = "rgba(" + red + "," + green + "," + blue + "," + "1.0)";
+    context.font = fontSize + "px " + fontface;
+
+    context.fillText(name, canvas.width/2, canvas.height/2);
+
+    // canvas contents will be used for a texture
+    var texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
+
+    //texture.needsUpdate = true;
+    var spriteMaterial = new THREE.SpriteMaterial({
+            //color: color,
+            transparent: false,
+            opacity: opacity,
+            useScreenCoordinates: false,
+            blending: THREE.AdditiveBlending,
+            map: texture}
+    );
+
+    var sprite = new THREE.Sprite(spriteMaterial);
+
+    sprite.scale.set(scale.x, scale.y, 1);
+    sprite.position.set(position.x, position.y, position.z);
+
+    return sprite;
+}
+
+$(document).ready(function() {
+    //Initialise app
+    var container = document.getElementById("WebGL-output");
+    var app = new EnergyApp();
+    app.init(container);
+    app.createScene();
+    app.createGUI();
+
+    //GUI callbacks
+    $("#chooseFile").on("change", function(evt) {
+        app.onSelectFile(evt);
+    });
+
+    app.run();
+});
