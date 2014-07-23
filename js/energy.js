7d2e1a3e49b8a668986e6259eecb8f55653eced2
DrMothra <tony.glover@nottingham.ac.uk>
2014-07-23 16:28:52 +0100

    Scene and gui updates
    Constructed intial scene and initial gui.

--- js/energy.js
+++ js/energy.js
@@ -5,10 +5,33 @@
 //Globals
 var GROUND_DEPTH = 240;
 var GROUND_WIDTH = 180;
+var SCREEN_WIDTH = 90;
+var SCREEN_HEIGHT = 40;
+
+function eliminateDuplicates(arr) {
+    var r = [];
+    start: for(var i = 0; i < arr.length; ++i) {
+        for(var x = 0; x < r.length; ++x) {
+            if(r[x]==arr[i]) {
+                continue start;
+            }
+        }
+        r[r.length] = arr[i];
+    }
+    return r;
+}
 
 //Init this app from base
 function EnergyApp() {
     BaseApp.call(this);
+}
+
+function getDate(timeDate) {
+    
+}
+
+function getTime(timeDate) {
+
 }
 
 EnergyApp.prototype = new BaseApp();
@@ -51,6 +74,8 @@ EnergyApp.prototype.createScene = function() {
 
     //Create ground
     addGroundPlane(this.scene, GROUND_WIDTH, GROUND_DEPTH);
+    //Create screen
+    addCinemaScreen(this.scene, SCREEN_WIDTH, SCREEN_HEIGHT);
 };
 
 EnergyApp.prototype.createGUI = function() {
@@ -90,22 +115,67 @@ EnergyApp.prototype.groundColourChanged = function(value) {
     }
 };
 
+/*
 EnergyApp.prototype.generateData = function() {
     //Parse data and rewrite in more suitable format
     var selected = ['hall_name_1', 'event_date_grouping', 'show_count_1', 'total_tkt_count_1', 'total_tkt_count_2'];
     var altered = ['hall_name', 'event_date', 'show_count', 'occupancy', 'admits'];
-    var newData = {};
+    var occupancy = [];
     for(var i=0; i<this.data.length; ++i) {
         //Only save selected data
+        var newData = {};
         var item = this.data[i];
         for(var key in item) {
             for(var j=0; j<selected.length; ++j) {
                 if(key == selected[j]) {
                     newData[altered[j]] = item[selected[j]];
+                    break;
                 }
             }
         }
+        occupancy.push(newData);
     }
+    //Save new data structure
+    var bb = window.Blob;
+    var filename = 'energy.json';
+    saveAs(new bb(
+            [JSON.stringify(occupancy)]
+            , {type: "text/plain;charset=" + document.characterSet}
+        )
+        , filename);
+};
+*/
+EnergyApp.prototype.generateData = function() {
+    //Extract data - do this manually for now
+    var item = this.data[0];
+    this.currentLocation = item['hall_name'];
+    this.currentDate = getDate(item['event_date']);
+    this.currentTime = getTime(item['event_date']);
+    this.locationNames = [];
+    this.maxOccupancy = [30, 345, 137, 105, 70];
+    this.days = ['Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu'];
+    this.maxDays = 30;
+
+    //Separate location names
+    for(var i=0; i<this.data.length; ++i) {
+        item = this.data[i];
+        this.locationNames.push(item['hall_name']);
+    }
+    //Remove duplicates
+    this.locationNames = eliminateDuplicates(this.locationNames);
+
+    //Generate occupancy visuals
+    var item = this.data[0];
+    populateInfoPanel(item);
+    populateHall(this.scene, item['admits']);
+};
+
+EnergyApp.prototype.showPreviousLocation = function() {
+
+};
+
+EnergyApp.prototype.showNextLocation = function() {
+
 };
 
 EnergyApp.prototype.parseFile = function() {
@@ -195,6 +265,64 @@ function addGroundPlane(scene, width, height) {
     scene.add(plane);
 }
 
+function addCinemaScreen(scene, width, height) {
+    //Create screen - simple box for now
+    var screenGeometry = new THREE.BoxGeometry(width, height, 5, 4, 4, 4);
+    var screenMaterial = new THREE.MeshLambertMaterial({color : 0x660000});
+    var screen = new THREE.Mesh(screenGeometry, screenMaterial);
+    screen.position.y = -60 + (height/2);
+
+    scene.add(screen);
+}
+
+function populateHall(scene, people) {
+    //Add number of people to hall
+    var radius = 2;
+    var personGeometry = new THREE.SphereGeometry(radius, 12, 12);
+    var personMaterial = new THREE.MeshLambertMaterial({color : 0x000066});
+
+    var startPos = new THREE.Vector3(-30, -60 + radius, 30);
+    var xInc = 5;
+    var zInc = 5;
+    for(var i=0; i<people; ++i) {
+        var person = new THREE.Mesh(personGeometry, personMaterial);
+        person.position.x = startPos.x;
+        person.position.y = startPos.y;
+        person.position.z = startPos.z;
+        scene.add(person);
+        startPos.x += xInc;
+    }
+}
+
+function populateInfoPanel(data) {
+    //Fill panel with relevant data
+
+    for(var key in data) {
+        var item = document.getElementById(key);
+        if (item) {
+            item.innerHTML = data[key];
+        }
+    }
+
+    //Remove any unnecessary data in headings
+    var title = data['hall_name'];
+    var end = title.indexOf('(');
+    if(end >= 0) {
+        title = title.substr(0, end);
+        document.getElementById('hall_name').innerHTML = title;
+    }
+
+    //Separate date and time
+    var date = data['event_date'];
+    var join = date.indexOf('-');
+    if(join >= 0) {
+        var day = date.substr(0, join-1);
+        document.getElementById('event_date').innerHTML = day;
+    }
+    var time = date.substr(join+1, date.length-join);
+    document.getElementById('event_time').innerHTML = time;
+}
+
 function createLabel(name, position, scale, colour, fontSize, opacity) {
 
     var fontface = "Arial";
@@ -256,6 +384,12 @@ $(document).ready(function() {
     //GUI callbacks
     $("#chooseFile").on("change", function(evt) {
         app.onSelectFile(evt);
+    });
+    $("#locationBackward").on("click", function(evt) {
+        app.showPreviousLocation();
+    });
+    $("#locationForward").on("click", function(evt) {
+        app.showNextLocation();
     });
 
     app.run();
